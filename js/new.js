a33d01e6197bf5ef48085bb40d09838bb5f7e59b
xishabh <62737828+xishabh@users.noreply.github.com>
2020-07-20 18:33:32 +0530

    added api fetch

--- js/new.js
+++ js/new.js
@@ -1,4 +1,33 @@
-$('#application-form').submit(function() {
-   var roll = $('#roll').val();
-   console.log(roll);
+
+var roll;
+
+function hello(e){
+console.log("hello");
+}
+
+function myFunction(roll_number) {
+
+//fetch("https://3b7c36c681d3.ngrok.io/student/1701046")
+
+  //.then(response => response.json())
+  //.catch(error => console.log('error', error));
+
+$.ajax({
+  url: "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6e14aa9b62dfb085e2b238e686b4d268",
+  data: '',
+  dataType: "json",
+  success: function(data){console.log(data)}
+})
+};
+
+
+$('#application-form').submit(function(event) {
+   //event.stopPropagation();
+   event.preventDefault();
+   roll = $('#roll').val();
+   console.log(roll)
+   myFunction(roll);
+
 });
+
+
