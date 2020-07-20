ec9b67d68df9bc2fa7705f9bd1e2f4200ba6ecf2
BakaZoro <56846056+BakaZoro@users.noreply.github.com>
2020-07-20 21:46:15 +0530

    Api changes

--- js/new.js
+++ js/new.js
@@ -1,33 +1,32 @@
-
+let student;
 var roll;
 
 function hello(e){
 console.log("hello");
 }
 
-function myFunction(roll_number) {
-
-//fetch("https://3b7c36c681d3.ngrok.io/student/1701046")
-
-  //.then(response => response.json())
-  //.catch(error => console.log('error', error));
-
-$.ajax({
-  url: "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6e14aa9b62dfb085e2b238e686b4d268",
-  data: '',
-  dataType: "json",
-  success: function(data){console.log(data)}
-})
-};
-
-
+//$(document).ready(function () {
+//$(document).on('click', 'input[name="Continue"]',function(event){
 $('#application-form').submit(function(event) {
    //event.stopPropagation();
    event.preventDefault();
    roll = $('#roll').val();
-   console.log(roll)
-   myFunction(roll);
+   window.location.href="./src/form.html?roll="+roll
+   //console.log(roll)
+   //myFunction(roll);
 
 });
+$('#registration-form').submit(function(event) {
+   //event.stopPropagation();
+   event.preventDefault();
+   roll = $('#roll').val();
+   sem=$('#Semester').val();
+   window.location.href="../src/subject.html?roll="+roll+"&semester="+sem
+   //console.log(roll)
+   //myFunction(roll);
 
+});
+function FillForm(){
+  document.getElementById("roll").value=roll
+}
 
