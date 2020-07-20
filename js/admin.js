c33ad1f8edae697e8ecbd74fa396646b3d3696a6
Prajawal Kumar Pandey <prajwalkp@hotmail.com>
2020-07-20 16:10:48 +0530

    updated

---
+++ js/admin.js
@@ -0,0 +1,20 @@
+$('input').click(function(){
+    var name=$(this).attr('name');
+    $("#back-div").css({
+		'height':10+"%"
+	});
+	$("#show").css({
+		'height':90+"%"
+	});
+	document.getElementById('heading').innerHTML=name;
+
+});
+
+$('#back').click(function(){
+	$("#back-div").css({
+		'height':0+"%"
+	});
+	$('#show').css({
+		'height':0+"%"
+	});
+});
